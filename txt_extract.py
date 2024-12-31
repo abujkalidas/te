197e7e085c201cac23c76cd98bef1caa14ea72d6
Kalidas Abuj <33632246+abujkalidas@users.noreply.github.com>
2024-12-31 05:41:16 +0530

    Update txt_extract.py

--- txt_extract.py
+++ txt_extract.py
@@ -1,13 +1,2 @@
-import cv2
-
-def list_cameras():
-    index = 0
-    while True:
-        cap = cv2.VideoCapture(index)
-        if not cap.isOpened():
-            break
-        print(f"Camera available at index {index}")
-        cap.release()
-        index += 1
-
-list_cameras()
+import streamlit as st
+st.camera_input("Test Camera")
